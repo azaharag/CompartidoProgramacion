bc90f3572d7fd4157c4225183a6d6650b8a1a692
Azahara Garrido <alejandriaxii@gmail.com>
2014-04-23 14:16:16 +0200

    Mejora de la clase FlujoYSwing con la opcion de cargar

--- Ev3/src/flujoEntradaSalida/FlujoYSwing.java
+++ Ev3/src/flujoEntradaSalida/FlujoYSwing.java
@@ -6,6 +6,8 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
 import javax.swing.JLabel;
 import javax.swing.JButton;
@@ -14,8 +16,12 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.PrintWriter;
+
 import javax.swing.JScrollPane;
+
 import java.awt.Font;
 import java.awt.Color;
 
@@ -75,18 +81,73 @@ public class FlujoYSwing extends JFrame {
 		btnGuardar.setFont(new Font("Tempus Sans ITC", Font.PLAIN, 20));
 		btnGuardar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				int confirmado = 0;
+				JFileChooser jfc=new JFileChooser();
+				jfc.setCurrentDirectory(new File("c:/"));
 				PrintWriter p = null;
+				jfc.showDialog(contentPane, "Seleccionar");
+				if((new File(jfc.getSelectedFile().getPath()).exists()))
+				{
+				 confirmado=JOptionPane.showConfirmDialog(contentPane, "¿Desea Sobreescribir?", "¿Desea Sobreescribir?", 
+					JOptionPane.YES_NO_OPTION,JOptionPane.WARNING_MESSAGE);
+				}
+				if(confirmado==JOptionPane.YES_OPTION)
+				{
 				try {
-					 p= new PrintWriter(new File("C:/Poema.txt"));
+					 p= new PrintWriter(jfc.getSelectedFile());
 				} catch (FileNotFoundException e1) {
 					e1.printStackTrace();
 				}
+				
 					p.println(textArea.getText());
 					p.close();
+				}
+				else
+				{
+						return;
+				}
 			}
 		});
-		btnGuardar.setBounds(258, 115, 103, 42);
+		btnGuardar.setBounds(256, 71, 103, 42);
 		contentPane.add(btnGuardar);
+		
+		JButton btnCargar = new JButton("Cargar");
+		btnCargar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e)  {
+				JFileChooser jfc=new JFileChooser();
+				jfc.setCurrentDirectory(new File("c:/"));
+				FileReader fr = null;
+				String t ="";
+				jfc.showDialog(contentPane, "Seleccionar");
+				try {
+					fr=new FileReader(jfc.getSelectedFile());
+					int c=fr.read();
+					while(c!=-1)
+					{ 
+						t=t+((char)c);
+						c=fr.read();
+					}
+					fr.close();
+					textArea.setText(t);
+				} catch (FileNotFoundException e1) {
+					JOptionPane.showMessageDialog(contentPane, "No se ha podido abrir: "+jfc.getSelectedFile().getPath());
+				} catch (IOException e1) {
+					JOptionPane.showMessageDialog(contentPane, "No se ha podido leer: "+jfc.getSelectedFile().getPath());
+						try {
+							fr.close();
+						} catch (IOException e2) {
+							// TODO Auto-generated catch block
+							e2.printStackTrace();
+						}
+				}
+				
+			}
+		});
+		btnCargar.setForeground(new Color(255, 160, 122));
+		btnCargar.setFont(new Font("Tempus Sans ITC", Font.PLAIN, 20));
+		btnCargar.setBackground(new Color(75, 0, 130));
+		btnCargar.setBounds(256, 143, 103, 42);
+		contentPane.add(btnCargar);
 	}
 	public JButton getBtnGuardar() {
 		return btnGuardar;
